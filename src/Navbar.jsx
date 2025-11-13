53e9175336162a454c8c3d1c2a91bb49846a561f
Arthur <arthur.felixa25@gmail.com>
2025-11-13 10:40:40 -0300

    style: imagem e botao arrumados

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,20 +2,20 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return(
-    <nav className="bg-[#f2f2f2] text-[#00008B] border-b-[1px] border-b-[#d2d2d2] flex justify-between items-center gap-8 py-0 px-4 font-bold text-[22px]">
+    <nav className="bg-[#f2f2f2] text-[#00008B] border-b-[1px] border-b-[#d2d2d2] flex justify-between items-center gap-8 py-3 px-6 font-bold text-[22px]">
             <Link to="/" className="text-[2rem]">Início</Link>
-            <ul className="m-0 p-0 list-none flex gap-4">
+            <ul className="m-0 p-0 list-none flex gap-4 items-center">
                 <li>
-                    <Link to="/info" className="flex no-underline h-full text-inherit items-center p-1 hover:bg-[#d2d2d2]">Informações</Link>
+                    <Link to="/info" className="text-[24px] flex no-underline h-full text-inherit items-center p-1 hover:bg-[#d2d2d2]">Informações</Link>
                 </li>
                 <li>
-                    <Link to="/register" className="flex no-underline h-full text-inherit items-center p-1 hover:bg-[#d2d2d2]">Cadastrar Golpe</Link>
+                    <Link to="/register" className="text-[24px] flex no-underline h-full text-inherit items-center p-1 hover:bg-[#d2d2d2]">Cadastrar Golpe</Link>
                 </li>
                 <li>
-                    <Link to="/empresas" className="flex no-underline h-full text-inherit items-center p-1 hover:bg-[#d2d2d2]">Empresas mais Usadas</Link>
+                    <Link to="/empresas" className="text-[24px] flex no-underline h-full text-inherit items-center p-1 hover:bg-[#d2d2d2]">Empresas mais Usadas</Link>
                 </li>
                 <li>
-                    <Link to="/rempresas" className="rounded-md bg-[#0b3d91] px-3 py-1.5 text-white font-semibold text-sm hover:bg-[#0e56bd]">Login Empresa</Link>
+                    <Link to="/rempresas" className="rounded-md bg-[#0b3d91] px-4 py-1 text-white font-semibold text-[24px] hover:bg-[#0e56bd] transition-colors flex items-center no-underline">Login Empresa</Link>
                 </li>
             </ul>
         </nav>
