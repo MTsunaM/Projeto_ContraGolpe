f2c5d1998961b6cb4e439a12e59d5e05d73b5741
Raphael Feijó <824135407@ulife.com.br>
2025-11-17 11:22:16 -0300

    feat: info screen updated

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,14 +4,134 @@ function Info() {
         <div className="bg-[url('https://userh.com.br/wp-content/uploads/2023/09/como-fazer-um-pdi.png')] bg-fixed bg-cover bg-center w-full h-[450px] flex items-center shadow-lg">
                 <div className="p-[50px]">
                     <div className="bg-white/50 backdrop-blur-sm rounded-md p-6 w-[600px] min-h-[180px]">
-                        <h1 className="text-left text-[#252525] text-[24px] font-bold leading-relaxed mb-3">Quer aprender a se previnir?</h1>
-                        <p className="text-left text-[#292929] text-[20px] font-semibold leading-relaxed">Venha conosco e aprenda a se proteger para não cair mais em golpes</p>
+                        <h1 className="text-left text-[#2e2e2e] text-[24px] font-extrabold leading-relaxed mb-3">Quer aprender a se previnir?</h1>
+                        <p className="text-left text-[#2e2e2e] text-[20px] font-bold leading-relaxed">Venha conosco e aprenda a se proteger para não cair mais em golpes</p>
                     </div>
                 </div>
             </div>
-            <div className="py-[55px]">
-            <h1 className="text-left text-[#000000] text-[30px] font-bold leading-relaxed">Aprenda em um passo a passo como se previnir de um golpe</h1>
-            <h2 className="text-left text-[#222121] text-[20px] font-semibold leading-relaxed py-[10px]">Passo: 1</h2>
+            <div className="py-[55px] mt-5">
+            <h1 className="text-left text-[#000000] text-[30px] font-extrabold leading-relaxed ml-2.5">⚠️ Aprenda em um passo a passo como se previnir de um golpe ⚠️</h1>
+
+            <div className="flex items-start justify-between">
+            <div className="flex flex-col">
+            <h2 className=" text-left text-[#00008b] text-[20px] font-bold leading-relaxed py-[10px] ml-5">Passo 1: Desconfie de presentes inesperados</h2>
+
+            <ul className="text-left text-[#2e2e2e] text-[18px] font-semibold leading-relaxed pl-5 ml-5">
+                <li>
+                      Se você não estava esperando nada, trate com cautela qualquer entrega surpresa.
+                </li>
+                <li>
+                      Golpistas costumam aparecer em datas comemorativas (Aniversário, Dia das Mães, Natal) para parecer mais convincente
+                </li>
+            </ul>
+
+            </div>
+
+             <img className="ml-5" src="https://www.acritica.com/image/policy:1.271747.1654351037:1654351037/image.jpg?f=default&w=1200" width={480} height={360}></img>
+            </div>
+            
+             
+             <div className="flex items-start justify-between">
+                <img className="mr-5" src="https://conteudo.imguol.com.br/c/noticias/af/2023/10/09/entregador-maquininha-entrega-cartao-de-debito-credito-golpe-cobranca-maquina-de-cartao-1696863736789_v2_900x506.jpg" width={480} height={360}></img>
+            <div className="flex flex-col">
+
+            <h2 className="text-right text-[#00008b] text-[20px] font-bold leading-relaxed py-[10px] mr-5">Passo 2: Nunca pague taxas na entrega</h2>
+
+            <ul className="text-right text-[#2e2e2e] text-[18px] font-semibold leading-relaxed pl-5 mr-5">
+                <li>
+                      Um dos sinais mais comuns do golpe é o entregador pedir uma taxa de entrega ou liberação.
+                </li>
+                <li>
+                      Nenhum presente legítimo exige pagamento na porta
+                </li>
+            </ul>
+            </div>
+
+            
+            </div>
+ 
+            <div className="flex items-start justify-between">
+            <div className="flex flex-col">
+            <h2 className="text-left text-[#00008b] text-[20px] font-bold leading-relaxed py-[10px] ml-5">Passo 3: Confirme a origem do presente</h2>
+
+            <ul className="text-left text-[#2e2e2e] text-[18px] font-semibold leading-relaxed pl-5 ml-5">
+                <li>
+                      Pergunte quem enviou e peça detalhes.
+                </li>
+                <li>
+                      Se possível, ligue para a pessoa ou empresa que supostamente enviou antes de aceitar
+                </li>
+            </ul>
+            </div>
+            <img className="mr-5" src="https://st2.depositphotos.com/2249091/11260/i/450/depositphotos_112602330-stock-photo-delivery-guy-waiting-for-client.jpg" width={430} height={310}></img>
+            </div>
+
+
+            <div className="flex items-start justify-between">
+                <img className="mr-5" src="https://www.meupositivo.com.br/doseujeito/wp-content/uploads/2022/04/RG-Unico-o-que-e-e-como-vai-funcionar-a-nova-carteira-de-identidade-1.jpg" width={480} height={360}></img>
+            <div className="flex flex-col">
+            <h2 className="text-right text-[#00008b] text-[20px] font-bold leading-relaxed py-[10px] ml-5">Passo 4: Não forneça dados pessoais </h2>
+
+            <ul className="text-right text-[#2e2e2e] text-[18px] font-semibold leading-relaxed pl-5 ml-5">
+                <li>
+                      Criminosos podem pedir CPF, RG ou até dados bancários para “registrar” a entrega.
+                </li>
+                <li>
+                      Nunca compartilhe informações sensíveis com desconhecidos
+                </li>
+            </ul>
+
+            </div>
+            </div>
+           
+            <div className="flex items-start justify-between">
+            <div className="flex flex-col">
+            <h2 className="text-left text-[#00008b] text-[20px] font-bold leading-relaxed py-[10px] ml-5">Passo 5: Verifique embalagens e notas fiscais </h2>
+
+            <ul className="text-left text-[#2e2e2e] text-[18px] font-semibold leading-relaxed pl-5 ml-5">
+                <li>
+                      Presentes verdadeiros geralmente vêm com nota fiscal ou comprovante de compra.
+                </li>
+                <li>
+                      A ausência desses documentos é um alerta
+                </li>
+            </ul>
+            </div>
+            <img className="mr-5" src="https://img.freepik.com/fotos-gratis/mulher-jovem-viajante-com-camiseta-branca-segurando-um-mapa-olhando-para-ele-com-uma-expressao-pensativa-no-rosto-pensando-em-pe-sobre-a-parede-laranja_141793-28962.jpg?semt=ais_incoming&w=740&q=80" width={480} height={360}></img>
+            </div>
+
+            
+            <div className="flex items-start justify-between">
+                <img className="mr-5" src="https://www.grupoiron.com/blog/wp-content/uploads/2024/01/porteiro-de-condominio.webp" width={480} height={360}></img>
+            <div className="flex flex-col">
+            <h2 className="text-right text-[#00008b] text-[20px] font-bold leading-relaxed py-[10px] ml-5">Passo 6: Prefira receber em locais seguros </h2>
+
+            <ul className="text-right text-[#2e2e2e] text-[18px] font-semibold leading-relaxed pl-5 ml-5">
+                <li>
+                      Se possível, peça para deixar na portaria ou em um ponto de coleta confiável.
+                </li>
+                <li>
+                      Evite contato direto com entregadores desconhecidos
+                </li>
+            </ul>
+            </div>
+            </div>
+            </div>
+
+            <div>
+                <h1 className="text-left text-[#00008b] text-[20px] font-bold leading-relaxed py-[10px] ml-5">
+                    Para mais informações, assista o video a seguir: 
+                </h1>
+                <iframe className="ml-5 mb-5"
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/UEtvjOq5UQE" 
+                title="GOLPE DO PRESENTE DE ANIVERSÁRIO - CUIDADO!" 
+                rameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+                    </iframe> 
             </div>
             </>
     )
